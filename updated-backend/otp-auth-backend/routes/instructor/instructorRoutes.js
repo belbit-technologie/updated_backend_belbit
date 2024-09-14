197911c75ab589978f7825b81fd81a907332f867
DHIRAN123 <nishu777777@gmail.com>
2024-09-14 12:19:33 +0530

    Resolved conflicts and merged changes from remote

--- updated-backend/otp-auth-backend/routes/instructor/instructorRoutes.js
+++ updated-backend/otp-auth-backend/routes/instructor/instructorRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { submitInstructorDetails } = require('../../controllers/Instructor/instructorController');
-
+const { submitInstructorDetails } = require('../../controllers/instructor/instructorController');
+const {removeInstructorDetails} = require('../../controllers/instructor/removeInstructorController');
 const router = express.Router();
 
 router.post('/submit-instructor-details', submitInstructorDetails);
-
+router.post('/remove-instructor', removeInstructorDetails);
 module.exports = router;
